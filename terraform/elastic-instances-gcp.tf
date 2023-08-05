18a88f3e3fbab05161d4f98a683a3b7d735c112b
Alvaro Andres Lopez Arias <37458753+alan89202@users.noreply.github.com>
2023-08-05 16:35:26 +0200

    Create elastic-instances-gcp.tf
    Terraform code to create servers on GCP

---
+++ terraform/elastic-instances-gcp.tf
@@ -0,0 +1,209 @@
+# Google configuration
+provider "google" {
+  credentials = file(var.credentials)
+  project     = var.project_name
+}
+
+# Master Logs disks creation
+resource "google_compute_disk" "master_logs_disks" {
+  count = var.master_count
+  name  = "master-logs-disk-${count.index}"
+  type  = "pd-standard"
+  size  = 30
+  zone = var.zone[count.index]
+}
+
+# master nodes disk creation
+resource "google_compute_disk" "master_disks" {
+  count = var.master_count
+  name  = "master-disk-${count.index}"
+  type  = "pd-standard"
+  size  = 50
+  zone = var.zone[count.index]
+}
+
+# VM instances creation - master nodes
+resource "google_compute_instance" "master_nodes" {
+  count        = var.master_count
+  name         = "master-node-${count.index}"
+  machine_type = var.master_machine_type
+  tags = var.elastic_tags 
+  allow_stopping_for_update = true
+  zone = var.zone[count.index]
+
+  boot_disk {
+    initialize_params {
+      image = var.image
+      size  = 50
+      type  = "pd-standard"
+    }
+  }
+
+  network_interface {
+    access_config {
+      network_tier = "PREMIUM"
+    }
+
+    subnetwork = "projects/alvaro-demo-elastic-000001/regions/us-west1/subnetworks/default"
+  }
+
+  attached_disk {
+    source = google_compute_disk.master_disks[count.index].self_link
+  }
+  attached_disk {
+    source = google_compute_disk.master_logs_disks[count.index].self_link
+  }
+}
+
+# Hot Logs disks creation
+resource "google_compute_disk" "hot_logs_disks" {
+  count = var.hot_count
+  name  = "hot-logs-disk-${count.index}"
+  type  = "pd-standard"
+  size  = 30
+  zone = var.zone[count.index]
+}
+
+# hot nodes disk creation
+resource "google_compute_disk" "hot_disks" {
+  count = var.hot_count
+  name  = "hot-disk-${count.index}"
+  type  = "pd-ssd"
+  size  = 200
+  zone = var.zone[count.index]
+}
+
+# VM instances creation - hot nodes
+resource "google_compute_instance" "hot_nodes" {
+  count        = var.hot_count
+  name         = "hot-node-${count.index}"
+  machine_type = var.hot_machine_type
+  tags = var.elastic_tags 
+  allow_stopping_for_update = true
+  zone = var.zone[count.index]
+
+  boot_disk {
+    initialize_params {
+      image = var.image
+      size  = 50
+      type  = "pd-standard"
+    }
+  }
+
+  network_interface {
+    access_config {
+      network_tier = "PREMIUM"
+    }
+
+    subnetwork = "projects/alvaro-demo-elastic-000001/regions/us-west1/subnetworks/default"
+  }
+
+  attached_disk {
+    source = google_compute_disk.hot_disks[count.index].self_link
+  }
+  attached_disk {
+    source = google_compute_disk.hot_logs_disks[count.index].self_link
+  }
+}
+
+# warm Logs disks creation
+resource "google_compute_disk" "warm_logs_disks" {
+  count = var.warm_count
+  name  = "warm-logs-disk-${count.index}"
+  type  = "pd-standard"
+  size  = 30
+  zone = var.zone[count.index + var.hot_count] 
+}
+
+# warm node disk creation
+resource "google_compute_disk" "warm_disk" {
+  count = var.warm_count
+  name = "warm-disk"
+  type = "pd-standard"
+  size = 1000
+  zone = var.zone[count.index + var.hot_count] 
+}
+
+# VM instances creation - warm nodes
+resource "google_compute_instance" "warm_node" {
+  count = var.warm_count
+  name = "warm-node"
+  machine_type = var.warm_machine_type
+  tags = var.elastic_tags
+  allow_stopping_for_update = true
+  zone = var.zone[count.index + var.hot_count]  
+
+  boot_disk {
+    initialize_params {
+      image = var.image
+      size  = 50
+      type  = "pd-standard"
+    }
+  }
+
+  network_interface {
+    access_config {
+      network_tier = "PREMIUM"
+    }
+
+    subnetwork = "projects/alvaro-demo-elastic-000001/regions/us-west1/subnetworks/default"
+  }  
+
+  attached_disk {
+    source = google_compute_disk.warm_disk[count.index].self_link
+  }
+  attached_disk {
+    source = google_compute_disk.warm_logs_disks[count.index].self_link
+  }
+}
+
+# kibana Logs disks creation
+resource "google_compute_disk" "kibana_logs_disks" {
+  count = var.kibana_count
+  name  = "kibana-logs-disk-${count.index}"
+  type  = "pd-standard"
+  size  = 30
+  zone = var.zone[1]
+}
+
+# kibana node disk creation
+resource "google_compute_disk" "kibana_disk" {
+  count = var.kibana_count
+  name = "kibana-disk"
+  type = "pd-standard"
+  size = 100
+  zone = var.zone[1]
+}
+
+# VM instances creation - kibana nodes
+resource "google_compute_instance" "kibana_node" {
+  count = var.kibana_count
+  name = "kibana-node"
+  machine_type = var.kibana_machine_type
+  tags = var.kibana_tags
+  allow_stopping_for_update = true
+  zone = var.zone[1] 
+
+  boot_disk {
+    initialize_params {
+      image = var.image
+      size  = 50
+      type  = "pd-standard"
+    }
+  }
+
+  network_interface {
+    access_config {
+      network_tier = "PREMIUM"
+    }
+
+    subnetwork = "projects/alvaro-demo-elastic-000001/regions/us-west1/subnetworks/default"
+  }
+
+  attached_disk {
+    source = google_compute_disk.kibana_disk[count.index].self_link
+  }
+  attached_disk {
+    source = google_compute_disk.kibana_logs_disks[count.index].self_link
+  }
+}
