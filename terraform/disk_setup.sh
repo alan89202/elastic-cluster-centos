160c2b2974d1b61e723f2e2e90dcd8a74a972bc4
Alvaro Andres Lopez Arias <37458753+alan89202@users.noreply.github.com>
2023-08-05 18:27:23 +0200

    Update disk_setup.sh

--- terraform/disk_setup.sh
+++ terraform/disk_setup.sh
@@ -1,35 +1,28 @@
 #!/bin/bash
-while [ "$(lsblk -l | grep 'sd[b-z]' | wc -l)" -lt 2 ]; do  # Cambia '2' al número de discos que esperas
-  echo "Waiting for disks to be ready..."
+
+# Contar los discos no montados al principio
+unmounted_disks=$(lsblk -dpno NAME,MOUNTPOINT | grep -c '^/dev/sd[b-z][^1-9]* $')
+
+n=1
+while [[ $unmounted_disks -gt 0 ]]; do
+  echo "Checking for unmounted disks..."
+  lsblk -dpno NAME,SIZE,MOUNTPOINT | while read -r dev size mountpoint; do
+    if [[ -z "$mountpoint" && "$dev" != "/dev/sda" ]]; then
+      if [[ $size == "30G" ]]; then
+        echo "Found unmounted disk $dev, size $size. Mounting to /applogs"
+        mount_point="/app/logs"
+      else
+        echo "Found unmounted disk $dev, size $size. Mounting to /mnt/data$n"
+        mount_point="/app/data$n"
+        n=$((n+1))
+      fi
+      parted $dev --script mklabel gpt mkpart xfspart xfs 0% 100%
+      mkfs.xfs ${dev}1
+      mkdir -p $mount_point
+      echo "${dev}1 $mount_point xfs defaults,nofail 0 2" | tee -a /etc/fstab
+      mount $mount_point
+      unmounted_disks=$((unmounted_disks-1))
+    fi
+  done
   sleep 10
 done
-
-lsblk -l -o NAME,SIZE,MOUNTPOINT | while read -r dev size mountpoint; do
-  # Skip lines that don't start with "sd"
-  if [[ "$dev" != sd* ]]; then continue; fi
-  # Skip the primary disk
-  if [[ "$dev" == sda* ]]; then continue; fi
-  # Skip if mountpoint is not empty
-  if [[ -n "$mountpoint" ]]; then continue; fi
-  # Remove the trailing 'G' from the size and convert to an integer
-  size=${size%G}
-  if [[ "$size" -eq 30 ]]; then
-    mount_dir="/applogs"
-  else
-    mount_dir="/appdata"
-  fi
-  echo "Found unmounted disk /dev/$dev, size ${size}G. Mounting to $mount_dir"
-  if [[ "$dev" == sd*[0-9] ]]; then
-    # If the device has a partition number, assume it is already formatted
-    mountpoint="/dev/$dev"
-  else
-    # If the device does not have a partition number, format it
-    parted /dev/$dev --script mklabel gpt mkpart xfspart xfs 0% 100%
-    mkfs.xfs /dev/${dev}1
-    mountpoint="/dev/${dev}1"
-  fi
-  mkdir -p $mount_dir
-  echo "$mountpoint $mount_dir xfs defaults,nofail 0 2" | sudo tee -a /etc/fstab
-  sudo mount $mount_dir
-done
-sudo reboot
