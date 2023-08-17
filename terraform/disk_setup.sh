fc0894c3090f1a81e3cacb33b0700b6ba3ac33e7
Alvaro Andres Lopez Arias <37458753+alan89202@users.noreply.github.com>
2023-08-17 09:10:12 +0200

    Update disk_setup.sh

--- terraform/disk_setup.sh
+++ terraform/disk_setup.sh
@@ -1,39 +1,41 @@
 #!/bin/bash
 
-unmounted_disks=$(lsblk -dpno NAME,MOUNTPOINT | grep -c '^/dev/sd[b-z][^1-9]* $')
-
+unmounted_disks=0
+for disk in $(lsblk -dpno NAME | grep '^/dev/sd[b-z]$'); do
+  partition="${disk}1"
+  if ! lsblk -pno NAME | grep -q "^$partition$"; then
+    unmounted_disks=$((unmounted_disks+1))
+  fi
+done
+echo $unmounted_disks
 n=1
 while [[ $unmounted_disks -gt 0 ]]; do
   echo "Checking for unmounted disks..."
-  lsblk -dpno NAME,SIZE,MOUNTPOINT | while read -r dev size mountpoint; do
-    
-    existing_partitions=$(lsblk -dpno NAME $dev | wc -l)
-    if [[ $existing_partitions -gt 1 ]]; then
-      echo "Disk $dev already has partitions."
-      continue
-    fi
-
-    if [[ -z "$mountpoint" && "$dev" != "/dev/sda" ]]; then
+  for disk in $(lsblk -dpno NAME,TYPE | grep 'disk' | awk '{print $1}'); do
+    partitions=$(lsblk -dpno NAME,MOUNTPOINT | grep ${disk})
+    if [[ ! $partitions =~ ${disk}1 && "$disk" != "/dev/sda" ]]; then
+      size=$(lsblk -dpno SIZE $disk | tr -d ' ')
       if [[ $size == "30G" ]]; then
-        echo "Found unmounted disk $dev, size $size. Mounting to /app/logs"
+        echo "Found unmounted disk $disk, size $size. Mounting to /app/logs"
         mount_point="/app/logs"
       else
-        echo "Found unmounted disk $dev, size $size. Mounting to /app/data$n"
+        echo "Found unmounted disk $disk, size $size. Mounting to /app/data$n"
         mount_point="/app/data$n"
         n=$((n+1))
       fi
-      parted $dev --script mklabel gpt mkpart xfspart xfs 0% 100%
+
+      parted $disk --script mklabel gpt mkpart xfspart xfs 0% 100%
       sleep 5
-      until [ -e "${dev}1" ]; do
+      until [ -e "${disk}1" ]; do
         sleep 1
       done
-      mkfs.xfs ${dev}1
+
+      mkfs.xfs ${disk}1
       mkdir -p $mount_point
-      echo "${dev}1 $mount_point xfs defaults,nofail 0 2" | tee -a /etc/fstab
+      echo "${disk}1 $mount_point xfs defaults,nofail 0 2" | tee -a /etc/fstab
       mount $mount_point
       unmounted_disks=$((unmounted_disks-1))
     fi
   done
   sleep 10
 done
-
