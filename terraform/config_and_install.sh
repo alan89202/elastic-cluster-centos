930379500c95b92c4b10eb28443fc4ba8d6380a5
Alvaro Andres Lopez Arias <37458753+alan89202@users.noreply.github.com>
2023-08-22 16:13:35 +0200

    Update config_and_install.sh

--- terraform/config_and_install.sh
+++ terraform/config_and_install.sh
@@ -1,3 +1,8 @@
+#Save terraform variables
+ES_KEYSTORE_PASS=$1
+ES_CA_PASS=$2
+INSTANCES_CERT_PASS=$3
+
 # Copy disk_setup.sh from the Cloud Storage bucket and set execution permissions
 gsutil cp gs://elk_config_files/disk_setup.sh /tmp/
 chmod +x /tmp/disk_setup.sh
@@ -19,6 +24,6 @@ else
   # If 'kibana' is not in the hostname, copy and execute basic_install_es.sh
   gsutil cp gs://elk_config_files/basic_install_es.sh /tmp/
   chmod +x /tmp/basic_install_es.sh
-  /tmp/basic_install_es.sh ${var.es_keystore_pass} ${var.es_ca_pass} ${var.instances_cert_pass}
+  /tmp/basic_install_es.sh $ES_KEYSTORE_PASS $ES_CA_PASS $INSTANCES_CERT_PASS
 fi
 
