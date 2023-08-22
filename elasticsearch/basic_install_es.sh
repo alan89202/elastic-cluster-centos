be6c121df3b9008c0848cbfc7a813e8af3d5520a
Alvaro Andres Lopez Arias <37458753+alan89202@users.noreply.github.com>
2023-08-22 13:51:05 +0200

    Update basic_install_es.sh

--- elasticsearch/basic_install_es.sh
+++ elasticsearch/basic_install_es.sh
@@ -83,8 +83,8 @@ sudo sed -i \
 -e '/#*path\.logs:/s/#*\([^:]*:\).*$/\1 \/app\/logs\/elasticsearch/' \
 -e '/#*network\.host:/s/#*\([^:]*:\).*$/\1 0.0.0.0/' \
 -e "/#*discovery\.seed_hosts:/s/#*\([^:]*:\).*$/\1 $seed_hosts/" \
--e '/xpack.security.transport.ssl:/,/xpack.security.transport.ssl:/\!b;s/\(^\s*keystore.path:\).*/\1 certs\/'"$myhostname"'.p12/' \
--e '/xpack.security.transport.ssl:/,/xpack.security.transport.ssl:/\!b;s/\(^\s*truststore.path:\).*/\1 certs\/'"$myhostname"'.p12/' \
+-e '/xpack.security.transport.ssl:/,/^\s*#/s/\(^\s*keystore.path:\).*/\1 certs\/'"$myhostname"'.p12/' \
+-e '/xpack.security.transport.ssl:/,/^\s*#/s/\(^\s*truststore.path:\).*/\1 certs\/'"$myhostname"'.p12/' \
 $FILE
 
 
